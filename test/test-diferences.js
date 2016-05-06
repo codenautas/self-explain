290df82b4dc4e294089c03c9a4cc4183d4d74cdc
Emilio <Platzer>
2016-05-06 18:05:46 -0300

    #1 diferencia de números

--- test/test-diferences.js
+++ test/test-diferences.js
@@ -11,15 +11,31 @@ var changing = require('best-globals').changing;
 [
     {functionName: 'diferences'   , strict:true },
     {functionName: 'bigDiferences', strict:false},
-].forEach(function(fixture){
-    describe("diferences with "+fixture.functionName, function(){
-        var diferences = assert[fixture.functionName];
+].forEach(function(mode){
+    describe("diferences with "+mode.functionName, function(){
+        it("equals inform null", function(){
+            var a={};
+            expect(diferences(a, a)).to.eql(null);
+        });
         it("inform diferent types", function(){
-            if(fixture.strict){
+            if(mode.strict){
                 expect(diferences(7, "7")).to.eql('typeof: number != string');
             }else{
                 expect(diferences(7, "7")).to.eql(null);
             }
+        });
+        var diferences = assert[mode.functionName];
+        [
+            {a: 0    , b:"0"  , expect:'typeof: number != string'         , expectNotStrict:null },
+            {a: 19021, b:19201, expect:-180                               },
+            {a: "1"  , b:0    , expect:'typeof: string != number'         , expectNotStrict:1},
+            {a: 1, b:0.999999 , skipped: '#1', expect:1-0.999999          , expectNotStrict:null},
+        ].forEach(function(fixture){
+            if(fixture.skipped){ return true; }
+            var expected = mode.strict || !('expectNotStrict' in fixture)?fixture.expect:fixture.expectNotStrict;
+            it("detect fixture "+fixture.message, function(){
+                expect(diferences(fixture.a, fixture.b)).to.eql(expected);
+            });
         });
     });
 });
