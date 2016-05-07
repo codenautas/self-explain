a0ce5b3c9ade35f6b20396286891e00711e4c9d7
Emilio <emilioplatzer@gmail.com>
2016-05-07 13:02:55 -0300

    acomodo el lugar de las opciones para las bigDiferences
    y separo las opciones de a una para poder cambiarlas

--- test/test-diferences.js
+++ test/test-diferences.js
@@ -8,33 +8,42 @@ var assert = selfExplain.assert;
 
 var changing = require('best-globals').changing;
 
+describe("diferences", function(){
+    it("inform allDiferences with mixed types", function(){
+        expect(assert.allDiferences(7, "7")).to.eql('typeof: number != string');
+    });
+    it("inform diferences opts.autoTypeCast with mixed types", function(){
+        expect(assert.diferences(7, "7", {autoTypeCast:true})).to.be(null);
+    });
+    it("inform bigDiferences with mixed types", function(){
+        expect(assert.bigDiferences(7, "7")).to.be(null);
+    });
+    it.skip/*#1*/("inform diferences opts.delta ", function(){
+        expect(assert.diferences(9.41, 9.418, {delta:0.01 })).to.be(null);
+        expect(assert.diferences(9.41, 9.418, {delta:0.001})).to.be(9.41-9.418);
+    });
+});
+
 [
-    {functionName: 'diferences'   , strict:true },
+    {functionName: 'allDiferences', strict:true },
     {functionName: 'bigDiferences', strict:false},
 ].forEach(function(mode){
     describe("diferences with "+mode.functionName, function(){
+        var diferences = assert[mode.functionName];
         it("equals inform null", function(){
             var a={};
-            expect(diferences(a, a)).to.eql(null);
+            expect(diferences(a, a)).to.be(null);
         });
-        it("inform diferent types", function(){
-            if(mode.strict){
-                expect(diferences(7, "7")).to.eql('typeof: number != string');
-            }else{
-                expect(diferences(7, "7")).to.eql(null);
-            }
-        });
-        var diferences = assert[mode.functionName];
         [
-            {a: 0    , b:"0"  , expect:'typeof: number != string'         , expectNotStrict:null },
-            {a: 19021, b:19201, expect:-180                               },
-            {a: "1"  , b:0    , expect:'typeof: string != number'         , expectNotStrict:1},
-            {a: 1, b:0.999999 , skipped: '#1', expect:1-0.999999          , expectNotStrict:null},
+            {a: 0     , b:"0"   , expect:'typeof: number != string'         , expectNotStrict:null },
+            {a: 19021 , b:19201 , expect:-180                               },
+            {a: "1"   , b:0     , expect:'typeof: string != number'         , expectNotStrict:1},
+            {a: 1, b:0.99999999 , skipped: '#1', expect:1-0.99999999        , expectNotStrict:null},
         ].forEach(function(fixture){
             if(fixture.skipped){ return true; }
             var expected = mode.strict || !('expectNotStrict' in fixture)?fixture.expect:fixture.expectNotStrict;
             it("detect fixture "+fixture.message, function(){
-                expect(diferences(fixture.a, fixture.b)).to.eql(expected);
+                expect(diferences(fixture.a, fixture.b)).to.be(expected);
             });
         });
     });
